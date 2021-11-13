3b5015ba61090c21352e1bc925c0765e06a3004a
trick track triu <tricktracktriu@mailbox.org>
2021-11-13 12:10:59 +0100

    Update

--- grafik.R
+++ grafik.R
@@ -13,25 +13,10 @@ library(readxl)
 
 ### plot
 
-# ggplot(data = filter(df, ubertritt == "sek"), 
-#        aes(fill = geschlecht , 
-#            y = ubertritt_total, 
-#            x = geschlecht)) +
-#            #x = filter(geschlecht == ends_with("_m")))) + # geschlecht
-#   geom_bar(position = "fill", 
-#            stat="identity") +
-#   #geom_text(aes(y = label_y, label = Weight), colour = "white")
-#   coord_flip() + 
-#   scale_y_continuous(labels = scales::percent) +
-#   theme_minimal() +
-#   theme(legend.position="none") +
-#   xlab(NULL) +
-#   ylab(NULL)
-
-ggplot(data = filter(df, geo_einh == "Jura bernois"), 
-       aes(fill = ubertritt, 
-           y = ubertritt_total, 
-           x = sprache)) +
+ggplot(data = filter(df, ubertritt == "sek"), 
+       aes(fill = geschlecht, 
+           y = geschlecht_wert, 
+           x = geschlecht)) + # geschlecht
   geom_bar(position = "fill", 
            stat="identity") +
   #geom_text(aes(y = label_y, label = Weight), colour = "white")
